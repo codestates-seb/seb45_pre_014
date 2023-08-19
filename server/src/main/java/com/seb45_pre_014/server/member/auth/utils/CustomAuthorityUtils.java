cf1083fe52aa5673ea6456cc484bdcd9cfa4d394
kihyun940 <houng027@gmail.com>
2023-08-20 07:28:00 +0900

    feat: Add h2-test

--- server/src/main/java/com/seb45_pre_014/server/member/auth/utils/CustomAuthorityUtils.java
+++ server/src/main/java/com/seb45_pre_014/server/member/auth/utils/CustomAuthorityUtils.java
@@ -14,18 +14,18 @@ public class CustomAuthorityUtils {
     @Value("${mail.address.admin}")
     private String adminMailAddress;
 
-    private final List<GrantedAuthority> ADMIN_ROLES = AuthorityUtils.createAuthorityList("ROLE_ADMIN", "ROLE_USER");
-    private final List<GrantedAuthority> USER_ROLES = AuthorityUtils.createAuthorityList("ROLE_USER");
+//    private final List<GrantedAuthority> ADMIN_ROLES = AuthorityUtils.createAuthorityList("ROLE_ADMIN", "ROLE_USER");
+//    private final List<GrantedAuthority> USER_ROLES = AuthorityUtils.createAuthorityList("ROLE_USER");
     private final List<String> ADMIN_ROLES_STRING = List.of("ADMIN", "USER");
     private final List<String> USER_ROLES_STRING = List.of("USER");
 
     // 메모리 상의 Role을 기반으로 권한 정보 생성.
-    public List<GrantedAuthority> createAuthorities(String email) {
-        if (email.equals(adminMailAddress)) {
-            return ADMIN_ROLES;
-        }
-        return USER_ROLES;
-    }
+//    public List<GrantedAuthority> createAuthorities(String email) {
+//        if (email.equals(adminMailAddress)) {
+//            return ADMIN_ROLES;
+//        }
+//        return USER_ROLES;
+//    }
 
     // DB에 저장된 Role을 기반으로 권한 정보 생성
     public List<GrantedAuthority> createAuthorities(List<String> roles) {
