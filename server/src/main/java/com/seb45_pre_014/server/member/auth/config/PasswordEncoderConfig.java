06ca593ad5e55ffe15d8aa6b9e65b56852f51df9
ararp1006 <ararp1006@gmail.com>
2023-08-21 17:31:07 +0900

    "gogle"

---
+++ server/src/main/java/com/seb45_pre_014/server/member/auth/config/PasswordEncoderConfig.java
@@ -0,0 +1,14 @@
+package com.seb45_pre_014.server.member.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordEncoderConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
