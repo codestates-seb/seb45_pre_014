81996d121063a2f3fba544a04a3bc0817d5322cf
kihyun940 <houng027@gmail.com>
2023-08-22 15:17:18 +0900

    Feat: test

--- server/src/main/java/com/seb45_pre_014/server/member/auth/handler/MemberAuthenticationSuccessHandler.java
+++ server/src/main/java/com/seb45_pre_014/server/member/auth/handler/MemberAuthenticationSuccessHandler.java
@@ -1,12 +1,17 @@
 package com.seb45_pre_014.server.member.auth.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import com.seb45_pre_014.server.exception.BusinessLogicException;
 import com.seb45_pre_014.server.exception.ExceptionCode;
 import com.seb45_pre_014.server.member.entity.Member;
 import com.seb45_pre_014.server.member.repository.MemberRepository;
 import com.seb45_pre_014.server.response.AuthResponse;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -21,11 +26,30 @@ public class MemberAuthenticationSuccessHandler implements AuthenticationSuccess
         public void onAuthenticationSuccess(HttpServletRequest request,
                                             HttpServletResponse response,
                                             Authentication authentication) throws IOException {
-            // 인증 성공 후, 로그를 기록하거나 사용자 정보를 response로 전송하는 등의 추가 작업을 할 수 있다.
+
+        Member member = (Member) authentication.getPrincipal();
+        successResponse(response, member);
+
+        }
+
+        private void successResponse(HttpServletResponse response, Member member) throws IOException{
+
+            Gson gson = new Gson();
+
+            LoginUser loginUser = new LoginUser();
+            loginUser.setMemberId(member.getMemberId());
+
+            response.setStatus(HttpStatus.OK.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().write(gson.toJson(loginUser, LoginUser.class));
 
 
-        log.info("# Authenticated successfully!");
-        System.out.println("로그인 성공");
+        }
+
+        @Getter
+        @Setter
+        private class LoginUser{
+          private long memberId;
         }
     }
 
